5064aaead8b4299b171b4de53f981e30121a8d14
mahmoudd2 <mahmouddahmedd220@gmail.com>
2024-05-02 18:27:26 +0300

    I-format w update 3ala int_to_binary

--- Final.c
+++ Final.c
@@ -43,7 +43,12 @@ char *opcode_to_binary(const char opcode []) {
     }
 }
 
-char *int_to_binary(int num, int num_bits) {
+
+
+// (1 << num_bits) deh bt shift left bel number of bits ya3ni lw el bits be 4 fa ha shift left hena 0001 be 4 fa htb2a 10000 el hya
+// equal 16 fa msln lw el num = -5 fa mfrod el binary representation bt3ha be equal 1011 el hya bardo equal lw 3mlna el -5 + 16 = 11
+// w gebna el binary representation bta3 el 11 el hwa = 1011  
+char *int_to_binary(int num, int num_bits, const char *type) {
     // Allocate memory for the binary string
     char *binary = (char *)malloc((num_bits + 1) * sizeof(char)); // +1 for the null terminator
     if (binary == NULL) {
@@ -51,16 +56,25 @@ char *int_to_binary(int num, int num_bits) {
         exit(1);
     }
 
-    // Ensure the number is within the supported range
-    if (num < 0 || num >= (1 << num_bits)) {  // 3ayzyn n check deh tany
-        fprintf(stderr, "Number out of range for %d bits\n", num_bits);
-        exit(1);
-    }
-
-    // Convert the integer to binary
-    for (int i = num_bits - 1; i >= 0; i--) {
-        int bit = (num >> i) & 1; // Extract each bit 3ayzyn nfhm deh aktar
-        binary[num_bits - 1 - i] = bit ? '1' : '0'; // Store '1' or '0'
+    if (strcmp(type, "IMM") == 0) { // 2's complement handling
+        if (num < 0) { // If negative, convert to 2's complement
+            num += (1 << num_bits);
+        }
+        for (int i = num_bits - 1; i >= 0; i--) {
+            if (num & (1 << i)) {
+                binary[num_bits - 1 - i] = '1'; // Set the bit to '1' if it's set in num
+            } else {
+                binary[num_bits - 1 - i] = '0'; // Set the bit to '0' if it's not set in num
+            }
+        }
+    } else { // Normal positive number handling
+        for (int i = num_bits - 1; i >= 0; i--) {
+            if (num & (1 << i)) {
+                binary[num_bits - 1 - i] = '1'; // Set the bit to '1' if it's set in num
+            } else {
+                binary[num_bits - 1 - i] = '0'; // Set the bit to '0' if it's not set in num
+            }
+        }
     }        
     binary[num_bits] = '\0'; // Null-terminate the string
 
@@ -117,6 +131,23 @@ char *concatenate_R_Format(const char *R1, const char *R2, const char*R3, const 
     strcat(concatenated,R2);
     strcat(concatenated,R3);
     strcat(concatenated,SHAMT);
+    return concatenated;
+}
+
+char *concatenate_I_Format(const char *R1, const char *R2, const char *IMM, const char* binary_opcode)
+{
+    char *concatenated =malloc(33);
+    if(concatenated == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);
+    }
+
+    strcpy(concatenated,binary_opcode);
+
+    strcat(concatenated,R1);
+    strcat(concatenated,R2);
+    strcat(concatenated,IMM);
     return concatenated;
 }
 
@@ -214,7 +245,7 @@ int main()
         if (strcmp(type, "J") == 0) {
             char *address = Words_array[1];
             int num = atoi(address);
-            address = int_to_binary(num,28); 
+            address = int_to_binary(num,28,"Address"); 
 
             // Call the function to concatenate
             binary_opcode = concatenate_J_Format(address, binary_opcode);
@@ -227,8 +258,8 @@ int main()
             char *Sec_reg = Words_array[2]+1;
             int num1 = atoi(First_reg);
             int num2 = atoi(Sec_reg);
-            First_reg = int_to_binary(num1,5);
-            Sec_reg = int_to_binary(num2,5);
+            First_reg = int_to_binary(num1,5,"Reg");
+            Sec_reg = int_to_binary(num2,5,"Reg");
             char *Third_reg;
             char *SHAMT;
             if (strcmp (Words_array[0], "LSL") == 0 || strcmp (Words_array[0], "LSR") == 0)
@@ -236,20 +267,35 @@ int main()
                 Third_reg = "00000";
                 SHAMT = Words_array[3];
                 int num3 = atoi(SHAMT);
-                SHAMT = int_to_binary(num3,13);            
+                SHAMT = int_to_binary(num3,13,"SHAMT");            
 
             }
             else
             {
                 Third_reg = Words_array[3]+1;
                 int num3 = atoi(Third_reg);
-                Third_reg = int_to_binary(num3,5);
+                Third_reg = int_to_binary(num3,5,"Reg");
                 SHAMT = "0000000000000";
             }
             binary_opcode = concatenate_R_Format(First_reg, Sec_reg, Third_reg, SHAMT, binary_opcode);
 
             printf("Final Binary Code Of The %d Instruction: %s\n\n", i + 1, binary_opcode);
 
+        }
+        else if (strcmp(type, "I") == 0)
+        {
+            char *First_reg = Words_array[1]+1;
+            char *Sec_reg = Words_array[2]+1;
+            int num1 = atoi(First_reg);
+            int num2 = atoi(Sec_reg);
+            First_reg = int_to_binary(num1,5,"Reg");
+            Sec_reg = int_to_binary(num2,5,"Reg");
+            char *IMM = Words_array[3];
+            int num3 = atoi(IMM);
+            IMM = int_to_binary(num3,18,"IMM");
+            binary_opcode = concatenate_I_Format(First_reg, Sec_reg, IMM, binary_opcode);
+
+            printf("Final Binary Code Of The %d Instruction: %s\n\n", i + 1, binary_opcode);
         }
         // Free memory for tokens
         for (int j = 0; j < current_pos; j++) {
--- TRIAL.c
+++ TRIAL.c
@@ -1,84 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 
-#define MAX_INSTRUCTIONS 1000
-#define MAX_LENGTH 100
+char *int_to_binary(int num, int num_bits, const char *type) {
+    // Allocate memory for the binary string
+    char *binary = (char *)malloc((num_bits + 1) * sizeof(char)); // +1 for the null terminator
+    if (binary == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);
+    }
 
-typedef struct {
-    char opcode[MAX_LENGTH];
-    char *operands; 
-} Instruction;
+    // Convert the integer to binary based on the type
+    if (strcmp(type, "IMM") == 0) { // 2's complement handling
+        if (num < 0) { // If negative, convert to 2's complement
+            num += (1 << num_bits);
+        }
+        for (int i = num_bits - 1; i >= 0; i--) {
+            if (num & (1 << i)) {
+                binary[num_bits - 1 - i] = '1'; // Set the bit to '1' if it's set in num
+            } else {
+                binary[num_bits - 1 - i] = '0'; // Set the bit to '0' if it's not set in num
+            }
+        }
+    } else { // Normal positive number handling
+        for (int i = num_bits - 1; i >= 0; i--) {
+            if (num & (1 << i)) {
+                binary[num_bits - 1 - i] = '1'; // Set the bit to '1' if it's set in num
+            } else {
+                binary[num_bits - 1 - i] = '0'; // Set the bit to '0' if it's not set in num
+            }
+        }
+    }
+
+    binary[num_bits] = '\0'; // Null-terminate the string
+
+    return binary;
+}
 
 int main() {
-    // // Open the assembly file for reading
-    // FILE *file = fopen("CA.txt", "r");
-    // if (file == NULL) {
-    //     fprintf(stderr, "Error opening file\n");
-    //     exit(1);
-    // }
+    // Test the function with different types
+    int num = -5; // Example 2's complement integer
+    int num_bits = 4; // Example number of bits
 
-    // // Array to store instructions
-    // Instruction instructions[MAX_INSTRUCTIONS];
-    // int num_instructions = 0;
+    char *binary_str = int_to_binary(num, num_bits, "IMM");
+    printf("Binary representation of %d (type IMM): %s\n", num, binary_str);
+    free(binary_str);
 
-    // // Read each line from the file
-    // char line[MAX_LENGTH];
-    // while (fgets(line, sizeof(line), file)) {
-    //     // Trim leading and trailing whitespace
-    //     char *trimmed_line = line;
-    //     while (isspace(*trimmed_line)) {
-    //         trimmed_line++;
-    //     }
-    //     char *end = trimmed_line + strlen(trimmed_line) - 1;
-    //     while (end > trimmed_line && isspace(*end)) {
-    //         *end-- = '\0';
-    //     }
-
-    //     // Check if the line is empty or contains only whitespace
-    //     if (*trimmed_line == '\0') {
-    //         continue;  // Skip empty lines
-    //     }
-
-    //     // Tokenize the line to extract opcode and operands
-    //     char *token = strtok(trimmed_line, " ");
-    //     if (token == NULL) {
-    //         fprintf(stderr, "Error parsing instruction\n");
-    //         exit(1);
-    //     }
-    //     strcpy(instructions[num_instructions].opcode, token);
-        
-    //     // Check if there are operands
-    //     token = strtok(NULL, "\n");
-    //     if (token == NULL || strlen(token) == 0) {
-    //         fprintf(stderr, "Error: Missing operands\n");
-    //         exit(1);
-    //     }
-        
-    //     // Dynamically allocate memory for operands
-    //     instructions[num_instructions].operands = malloc(strlen(token) + 1);
-    //     if (instructions[num_instructions].operands == NULL) {
-    //         fprintf(stderr, "Error allocating memory\n");
-    //         exit(1);
-    //     }
-    //     strcpy(instructions[num_instructions].operands, token);
-
-    //     num_instructions++;
-    // }
-
-    // // Close the file
-    // fclose(file);
-
-    // // Print the parsed instructions
-    // printf("Parsed Instructions:\n");
-    // for (int i = 0; i < num_instructions; i++) {
-    //     printf("%s %s\n", instructions[i].opcode, instructions[i].operands);
-    //     // Don't forget to free dynamically allocated memory
-    //     free(instructions[i].operands);
-    // }
-    char string[] = "hello \n";
-    printf("%s",string);
+    num = 5; // Example positive integer
+    binary_str = int_to_binary(num, num_bits, "POS");
+    printf("Binary representation of %d (type POS): %s\n", num, binary_str);
+    free(binary_str);
 
     return 0;
 }
